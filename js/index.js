f5fa10524c258c0a1db3f8b0c6d6f0eed332440c
LilDrikks <87108295+LilDrikks@users.noreply.github.com>
2021-10-04 16:07:32 -0300

    up4.0Site

--- js/index.js
+++ js/index.js
@@ -1,15 +1,3 @@
-const btn = document.getElementById('send')
-
-btn.addEventListener('click', () => {
-    btn.classList.add('is-loading')
-
-
-    setTimeout(() => {
-        btn.classList.add('is-success')
-        btn.classList.remove('is-loading')
-    }, 4000)
-})
-
 //variavel de escopo global
 let btnMobile = document.getElementById('btn-mobile')
 
